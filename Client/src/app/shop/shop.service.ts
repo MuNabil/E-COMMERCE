16e8eee613d4164ec9c699ff80ce7df902801879
Mohamed Nabil <nabilhekal8@gmail.com>
2023-03-06 02:59:33 +0200

    Client Routing

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IType } from '../shared/models/productType';
 import { IPagination } from '../shared/models/pagination';
 import { map } from 'rxjs';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ import { ShopParams } from '../shared/models/shopParams';
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) { }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
 
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
